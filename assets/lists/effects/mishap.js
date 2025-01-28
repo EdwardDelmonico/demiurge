b27fc71d791ac99ac81f84e53f0e431e5b9ada33
Edward Delmonico <ed.delmonico@gmail.com>
2025-01-28 18:34:26 +0200

    protect da copyright

--- assets/lists/effects/mishap.js
+++ assets/lists/effects/mishap.js
@@ -1,7 +1,22 @@
 const mishaps = [
     'Process explodes, inflicting damage equal to the desired item level on all nearby.',
     'Location where mishap occurs is seriously damaged, requiring ten weeks of repair costing 500 crystal orbs.',
-    'this is the last mishap'
+    'Item gains intelligence and deep hatred for Maker. It teleports away, vowing vengeance.',
+    'Nearest magical item is drained permanently',
+    'Maker becomes disfigured and unsightly',
+    'Maker gains the attention of a powerful entity',
+    'Maker loses all connection to their secret soul and their soul name changes',
+    'Maker must attempt to resist possession by a level 6 demon',
+    'Portal to the Dark opens',
+    'Maker is cursed with a random curse spell effect',
+    'Maker\’s hand is burned and cannot be used for one week afterward',
+    'Maker is struck blind for one week',
+    'Item functions exactly as originally desired, but becomes a possession of the Maker’s worst enemy',
+    'Item is possessed by a level 8 demon that attempts to corrupt its use',
+    'Angel appears and demands possession of the item',
+    'Three angry level 5 ghosts appear and attack Maker',
+    'Maker is afflicted with a debilitating disease that gives a scourge in a random stat pool that remains until the disease is magically cured',
+    'Maker is hurled to another world or realm'
 ]
 
 module.exports = mishaps
